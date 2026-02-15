51b1abbceedf44147a08d030374cbe1161f79bd4
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-02-15 16:20:29 +0400

    ..

--- main.c
+++ main.c
@@ -5,45 +5,47 @@
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t pid;
-    int status;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status;
+	char *args[2];
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            free(line);
-            exit(0);
-        }
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			exit(0);
+		}
 
-        if (line[read - 1] == '\n')
-            line[read - 1] = '\0';
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
 
-        if (line[0] == '\0')
-            continue;
+		if (line[0] == '\0')
+			continue;
 
-        pid = fork();
-        if (pid == 0)
-        {
-            char *args[] = {line, NULL};
+		pid = fork();
+		if (pid == 0)
+		{
+			args[0] = line;
+			args[1] = NULL;
 
-            execve(line, args, NULL);
-            perror("./hsh");
-            exit(1);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+			execve(line, args, NULL);
+			perror("./hsh");
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
