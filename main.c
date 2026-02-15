31601ad978f956f1cd7b980e3e8cfec1e4ce6c41
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-02-15 16:19:29 +0400

    change main.c

--- main.c
+++ main.c
@@ -1,110 +1,49 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/**
- * trim - removes leading and trailing spaces
- * @str: input string
- * Return: trimmed string
- */
-char *trim(char *str)
+int main(void)
 {
-	char *end;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    int status;
 
-	while (*str == ' ' || *str == '\t')
-		str++;
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "$ ", 2);
 
-	if (*str == 0)
-		return (str);
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            free(line);
+            exit(0);
+        }
 
-	end = str + strlen(str) - 1;
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
 
-	while (end > str && (*end == ' ' || *end == '\t' || *end == '\n'))
-		end--;
+        if (line[0] == '\0')
+            continue;
 
-	*(end + 1) = '\0';
+        pid = fork();
+        if (pid == 0)
+        {
+            char *args[] = {line, NULL};
 
-	return (str);
-}
+            execve(line, args, NULL);
+            perror("./hsh");
+            exit(1);
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
 
-/**
- * execute_command - executes a command using fork and execve
- * @command: command to execute
- * @prog_name: name of shell program
- * @line_count: current line number
- */
-void execute_command(char *command, char *prog_name, int line_count)
-{
-	pid_t pid;
-	int status;
-	char *args[2];
-
-	if (access(command, X_OK) == -1)
-	{
-		fprintf(stderr, "%s: %d: %s: not found\n",
-				prog_name, line_count, command);
-		return;
-	}
-
-	pid = fork();
-
-	if (pid == 0)
-	{
-		args[0] = command;
-		args[1] = NULL;
-
-		execve(command, args, environ);
-		perror(prog_name);
-		exit(EXIT_FAILURE);
-	}
-	else if (pid > 0)
-	{
-		wait(&status);
-	}
-	else
-	{
-		perror("fork");
-	}
-}
-
-/**
- * main - simple shell entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
-int main(int argc, char **argv)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	int line_count = 0;
-
-	(void)argc;
-
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("($) ");
-
-		read = getline(&line, &len, stdin);
-
-		if (read == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				printf("\n");
-			break;
-		}
-
-		line_count++;
-
-		line[strcspn(line, "\n")] = '\0';
-		line = trim(line);
-
-		if (line[0] == '\0')
-			continue;
-
-		execute_command(line, argv[0], line_count);
-	}
-
-	free(line);
-	return (0);
+    free(line);
+    return (0);
 }
